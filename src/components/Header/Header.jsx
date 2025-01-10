63b8d502f686ea14b1bb6d1c5a306523da900b6e
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-10 17:35:47 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <p className="text-5xl">Hi</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Header from './components/Header/Header';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
