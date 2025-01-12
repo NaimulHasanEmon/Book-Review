fb712e6109b040de4d4653099d91e78b53f237da
Md. Naimul Hasan Emon <hasan15-5601@diu.edu.bd>
2025-01-12 20:53:07 +0600

    Responsive Read Chart

--- src/components/PagesToReadChart/PagesToReadChart.jsx
+++ src/components/PagesToReadChart/PagesToReadChart.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Line } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'rebeccapurple'];
 
-const PagesToReadChart = ({books}) => {
+const PagesToReadChart = ({ books }) => {
     const data = books.map((book) => ({
         bookName: book.bookName,
         totalPages: book.totalPages,
@@ -19,24 +19,22 @@ const PagesToReadChart = ({books}) => {
     const TriangleBar = (props) => {
         const { fill, x, y, width, height } = props;
         return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-        };
+    };
 
     return (
-        <div>
-            <BarChart
-                width={900}
-                height={300}
-                data={data}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="bookName" />
-                <YAxis />
-                <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                    {data.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                    ))}
-                </Bar>
-            </BarChart>
+        <div className="w-full h-72">
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="bookName" />
+                    <YAxis />
+                    <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                        ))}
+                    </Bar>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
